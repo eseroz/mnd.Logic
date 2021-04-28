1eb64f8d21af5f60de8d2c2312d02a7849ce95b2
Eser Öznohut <esero@outlook.com.tr>
2021-04-28 23:49:52 +0300

    p
    p

--- BC_Satis/_PotansiyelDisi/PotansiyelDisiMusteriArama.cs
+++ BC_Satis/_PotansiyelDisi/PotansiyelDisiMusteriArama.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mnd.Logic.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,25 +8,43 @@ using System.Threading.Tasks;
 
 namespace mnd.Logic.BC_Satis._PotansiyelDisi
 {
-    public class PotansiyelDisiMusteriArama
+    public class PotansiyelDisiMusteriArama : MyBindableBase
     {
+        private string gorusulenKisiTelefon;
+        private string gorusulenKisiGorevi;
+        private string gorusulenKisiAdi;
+        private string konu;
+        private string konuDetay;
+        private string ulkeKodu;
+        private string ulkeAdi;
+        private string musteriGrubuAdı;
+        private string musteriUnvan;
+        private DateTime? tarih;
+        private string gorusulenKisiEposta;
+        private string ekleyen;
+        private string plasiyerKod;
+
         [Key]
         public int Id { get; set; }
-        public DateTime? Tarih { get; set; }
-        public string MusteriUnvan { get; set; }
-        public string MusteriGrubuAdı { get; set; }
-        public string UlkeAdi { get; set; }
-        public string UlkeKodu { get; set; }
-        public string KonuDetay { get; set; }
-        public string Konu { get; set; }
-        public string GorusulenKisiAdi { get; set; }
-        public string GorusulenKisiGorevi { get; set; }
-        public string GorusulenKisiTelefon { get; set; }
-        public string GorusulenKisiEposta { get; set; }
+        public DateTime? Tarih { get => tarih; set => SetProperty(ref tarih, value); }
+        public string MusteriUnvan { get => musteriUnvan; set => SetProperty(ref musteriUnvan, value); }
+        public string MusteriGrubuAdı { get => musteriGrubuAdı; set => SetProperty(ref musteriGrubuAdı, value); }
+        public string UlkeAdi { get => ulkeAdi; set => SetProperty(ref ulkeAdi, value); }
+        public string UlkeKodu { get => ulkeKodu; set => SetProperty(ref ulkeKodu, value); }
+        public string KonuDetay { get => konuDetay; set => SetProperty(ref konuDetay, value); }
+        public string Konu { get => konu; set => SetProperty(ref konu, value); }
+        public string GorusulenKisiAdi { get => gorusulenKisiAdi; set => SetProperty(ref gorusulenKisiAdi, value); }
+        public string GorusulenKisiGorevi { get => gorusulenKisiGorevi; set => SetProperty(ref gorusulenKisiGorevi, value); }
+        public string GorusulenKisiTelefon
+        {
+            get => gorusulenKisiTelefon;
+            set => SetProperty(ref gorusulenKisiTelefon, value);
+        }
+        public string GorusulenKisiEposta { get => gorusulenKisiEposta; set => SetProperty(ref gorusulenKisiEposta, value); }
 
-        public string Ekleyen { get; set; }
+        public string Ekleyen { get => ekleyen; set => SetProperty(ref ekleyen, value); }
 
-        public string PlasiyerKod { get; set; }
+        public string PlasiyerKod { get => plasiyerKod; set => SetProperty(ref plasiyerKod, value); }
 
     }
 }
